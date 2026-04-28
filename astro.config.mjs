87c12877339f15b806e7a1df7ea58c4d653ed39d
A. Abqory <215096301+abqoryme@users.noreply.github.com>
2026-04-28 19:10:21 +0700

    Update Vercel adapter configuration and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), compress()],
   output: "server",
-  adapter: vercel()
+
+  adapter: vercel({
+    runtime: "nodejs22.x"
+  }),
+
+  integrations: [
+    tailwind(),
+    icon(),
+    compress()
+  ]
 });
